fc4b7be7394ef57b66d11370f45b4457133b70bf
darigan17 <zerglinglover@gmail.com>
2016-04-08 16:34:45 -0400

    ball game

---
+++ Ball Game/Assets/Scripts/playerMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+    public float speed;
+    private float prevH=0,prevV;
+    private float HorV, VertV;
+    private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if(gameObject.tag == "player1")
+        {
+            
+            HorV = Input.GetAxis("Horizontal");
+            VertV = Input.GetAxis("Vertical");
+        }
+        else if (gameObject.tag == "player2")
+        {
+            HorV = Input.GetAxis("Horizontal2");
+            VertV = Input.GetAxis("Vertical2");
+
+        }
+
+        Vector3 force = new Vector3(speed * HorV, 0, speed * VertV);
+        rb.AddForce(force);
+    }
+}
